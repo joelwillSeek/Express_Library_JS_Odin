bd1ba9fa1d3a37224c6085d46396bfb37687c1f2
joel <joelthousend@gmail.com>
2024-05-26 18:59:15 +0300

    added some fix in app.js

--- app.js
+++ app.js
@@ -36,7 +36,19 @@ app.use("/catalog", catalogRoutes);
 app.use("/users", userRoutes);
 app.use("/", indexRoutes);
 
-app.listen(port, () => {
-  //replace with debug
-  console.log("Server started on port " + port);
+app.use(function (req, res, next) {
+  next(createError(404));
 });
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render("error");
+});
+
+module.exports = app;
